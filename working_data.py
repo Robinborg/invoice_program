856bfc1adf230e4cb36e5fca1d5eed709208c00a
Robinborg <coalchewer@robins-macbook-air.home>
2021-10-11 07:10:26 +0200

    tweaking the adding to server

--- working_data.py
+++ working_data.py
@@ -3,40 +3,53 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
+from typing import List
 
 
 class FetchAndTransform:
     def __init__(self):
         '''creating the engine and fetching the current products and customers'''
-        engine = create_engine('sqlite:///invoice.db')
-        self.products = pd.read_sql('products', engine)
-        self.customers = pd.read_sql('customers', engine)
+        self.engine = create_engine('sqlite:///invoice.db')
+        self.products = pd.read_sql('products', self.engine)
+        self.customers = pd.read_sql('customers', self.engine)
 
-    def working_table(self, add_product=None, add_customer=None):
+    def working_table(self, product_number=0) -> List:
         '''loading the data into a table for the invoice'''
+        if product_number:
+            add_table_products = self.products.iloc[product_number]
         table_data = [
-                product,
-                customer,
+                add_table_products,
             ['Serial', 'Goods and description', 'Rate', 'QTY', 'Total'],       
             ]
         return table_data
 
-    def adding_to_database(self,new_prouct=None, new_customer=None):
+    def adding_customer_details(self, customer=0):
+        pass
+
+
+    def adding_to_database(self, new_product=None, new_customer=None):
         if new_product:
-            product_number = self.products.iloc[-1, -1] + 1
-            product_name = product
-            products[product_name] = product_number
+            product_number =  1
+            product_name = new_product
+            product_combined = pd.DataFrame({"name": [product_name], "product number": [product_number]}, index=[product_number])
+            self.products.append(product_combined, ignore_index=True)
+
+            self.products.to_sql('products', self.engine, if_exists='append')
+            print(self.products)
+
         elif new_customer:
-            customer_number = self.customers.iloc[-1, -1] + 1
-            customer_name = customer
-            customers[customer_name] = customer_number
+            customer_number = 1
+            customer_name = new_customer
+            customers_combined = pd.DataFrame({"name": [customer_name], "customer number": [customer_number]}, index=[customer_number])
+            self.customers.append(customers_combined, ignore_index=True)
+            
+            self.customers.to_sql('customers', self.engine, if_exists='append')
+            print(self.customers)
+
         else:
             print("Enter product=your_choice or customer = your_choice")
 
-        self.products.to_sql('products', self.engine)
-        self.customers.to_sql('customers', self.engine)
-
-    def removing_row(self, remove_product=None, remove_customer=None):
+    def removing_row(self, remove_product=None, remove_customer=None, delete_all=None):
         '''removing products or customers'''
         if remove_product == "product":
             del self.products['product2']
@@ -48,12 +61,22 @@ class FetchAndTransform:
         elif remove_customer == "customer":
             self.customers.drop(len(self.customers)-1)
             print(self.customers)
+        elif delete_all:
+            self.customers = pd.DataFrame()
+            self.products = pd.DataFrame()
         else:
             print("Enter remove_product = your_choice or remove_customer = your_choice")
 
-        self.customers.to_sql('customers', engine)
-        self.products.to_sql('products', engine)
+        self.customers.to_sql('customers', self.engine, if_exists='replace')
+        self.products.to_sql('products', self.engine, if_exists='replace')
+        
 
+    def show_table(self):
+        '''print the whole table from sqlite3'''
+        self.products
+        print(self.products)
+        self.customers
+        print(self.customers)
 
 
 
