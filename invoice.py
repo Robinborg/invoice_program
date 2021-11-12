d33620fa2de153a77a927d8270b575f3e1ec78ff
Robinborg <coalchewer@robins-macbook-air.home>
2021-11-12 17:42:07 +0100

    Made the whole invoice larger

--- invoice.py
+++ invoice.py
@@ -14,21 +14,22 @@ class InvoiceTemplate:
     """The invoice template"""
     def __init__(self, invoice_number: int):
         """Start the template with a Canvas and get the today's date"""
-        self.PAGE_SIZE = (595.27, 841.90)
+        self.PAGE_SIZE = A4 #(595.27, 841.90)
         self.c = canvas.Canvas(invoice_number + ".pdf", pagesize=self.PAGE_SIZE, bottomup=0)
         self.width, self.height = A4
         self.date = datetime.today().strftime('%d-%m-%Y')
 
     def make_data_table(self, data_table: List):
         """Create the Table for the products"""
-        self.style = TableStyle([
-            ('BOX', (0,0), (-1,-1), 0.25, colors.black),
-            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
-            ('ALIGN', (0,-1), (-1,-1), "CENTER"),
-            ('VALIGN', (3,3), (-1,-1), "BOTTOM"),
-            ('FONTSIZE', (0,0), (-1,-1), 5),
-            ])
-        self.table = Table(data_table, style=self.style)
+        first_table = Table(data_table, rowHeights=2*cm, colWidths=4*cm)
+        first_table.setStyle(TableStyle([
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+            ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+            ]))
+        self.table = first_table
+         
 
     def create_document(self, invoice_number=0, customer_name: str = None, customer_phone: int = 0):
         """Create the invoice canvas"""
@@ -36,34 +37,30 @@ class InvoiceTemplate:
         self.c.translate(10, 40)
         self.c.scale(1, -1)
         self.c.scale(1, -1)
-        self.c.setFont("Helvetica-Bold", 5)
-        self.c.drawCentredString(30, -1, "Company Ab Oy")
-        self.c.setFont("Helvetica-Bold", 2)
-        self.c.drawCentredString(30, 2, "Address: company's street 1")
-        self.c.drawCentredString(30, 6, "Postal: number 123")
-        self.c.setFont("Helvetica-Bold", 2)
-        self.c.drawCentredString(30, 10, "Business ID: 999999-9")
-        self.c.line(5, 15, 250, 15)
+        self.c.setFont("Helvetica-Bold", 20)
+        self.c.drawCentredString(400, 10, "Invoice")
+        self.c.setFont("Helvetica-Bold", 15)
+        self.c.drawCentredString(130, 1, "Company Ab Oy")
+        self.c.setFont("Helvetica-Bold", 15)
+        self.c.drawCentredString(130, 20, "Address: company's street 1")
+        self.c.drawCentredString(130, 40, "Postal: number 123")
+        self.c.setFont("Helvetica-Bold", 15)
+        self.c.drawCentredString(130, 60, "Business ID: 999999-9")
         #Middle section 
-        self.c.setFont("Courier-Bold", 10)
-        self.c.drawCentredString(80, -10, "Invoice")
-        self.c.roundRect(5, 23, 170, 40, 10, stroke=1, fill=0)
-        self.c.setFont("Times-Bold", 3)
-        self.c.drawRightString(45, 30, f"Invoice number: {invoice_number}" )
-        self.c.drawRightString(45, 40, f"Date: {self.date} ")
-        self.c.drawRightString(45, 50, f"Customer name: {customer_name} ")
-        self.c.drawRightString(45, 60, f"Phone number: {customer_phone}")
+        self.c.roundRect(5, 80, 570, 100, 10, stroke=1, fill=0)
+        self.c.setFont("Times-Bold", 12)
+        self.c.drawRightString(200, 100, f"Invoice number: {invoice_number}" )
+        self.c.drawRightString(200, 120, f"Date: {self.date} ")
+        self.c.drawRightString(200, 140, f"Customer name: {customer_name} ")
+        self.c.drawRightString(200, 160, f"Phone number: {customer_phone}")
         #Table
         table_info = self.table
         table_info.wrapOn(self.c, self.width, self.height)
-        table_info.drawOn(self.c, 0*mm, 30*mm)
+        table_info.drawOn(self.c, -1*mm, 80*mm)
         #Bottom section
-        self.c.line(15, 220, 185, 220)
-        self.c.line(100, 220, 100, 238)
-        self.c.drawString(20, 225, "We declare that above mentioned")
-        self.c.drawString(20, 230, "information is true")
-        self.c.drawString(20, 235, "(This system generated invoice)")
-        self.c.drawRightString(180, 235, "Authorised signatory")
+        self.c.setFont("Helvetica-Bold", 8)
+        self.c.drawString(20, 780, "We declare that above mentioned information is true")
+        self.c.drawString(20, 790, "(This system generated invoice)")
         self.c.showPage()
 
     def save_pdf(self):
--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 from invoice import InvoiceTemplate
 from working_data import FetchAndTransform
-
+#Make default number
 if __name__ == "__main__":
-    #Make default and number from somewhere first_invoice = InvoiceTemplate("2")
+    first_invoice = InvoiceTemplate("2")
     modify_data = FetchAndTransform()
     make_table = modify_data.working_table(select_index=1, quantity=2)
     #print(make_table)
@@ -17,22 +17,22 @@ if __name__ == "__main__":
     first_invoice.create_document(invoice_number=2,
             customer_name = customer_details[0], customer_phone = customer_details[2])
     first_invoice.save_pdf()
-    while True:
-        enter_command = input("Enter instruction: ")
-        if enter_command.lower() == 'quit':
-            break
-        elif enter_command.lower() == "add product":
-            add_product_serial= input("Enter product serial: ")
-            add_product_goods_description = input("Enter product goods and description: ")
-            add_product_rate= input("Enter product rate: ")
-            modify_data.adding_product_details(add_product_serial,
-                    add_product_goods_description, add_product_rate)
-            continue
-        elif enter_command.lower() == 'add customer':
-            add_customer_name = input("Enter customer name: ")
-            add_customer_address = input("Enter customer address: ")
-            add_customer_phone = input("Enter customer phone: ")
-            modify_data.adding_customer_details(add_customer_name, add_customer_address,
-                    add_customer_phone)
-            continue
-
+#    while True:
+#        enter_command = input("Enter instruction: ")
+#        if enter_command.lower() == 'quit':
+#            break
+#        elif enter_command.lower() == "add product":
+#            add_product_serial= input("Enter product serial: ")
+#            add_product_goods_description = input("Enter product goods and description: ")
+#            add_product_rate= input("Enter product rate: ")
+#            modify_data.adding_product_details(add_product_serial,
+#                    add_product_goods_description, add_product_rate)
+#            continue
+#        elif enter_command.lower() == 'add customer':
+#            add_customer_name = input("Enter customer name: ")
+#            add_customer_address = input("Enter customer address: ")
+#            add_customer_phone = input("Enter customer phone: ")
+#            modify_data.adding_customer_details(add_customer_name, add_customer_address,
+#                    add_customer_phone)
+#            continue
+#
