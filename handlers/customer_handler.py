636f1c776e86ea41a79100f21d763acd55242254
Robinborg <coalchewer@robins-macbook-air.home>
2021-11-20 17:59:25 +0100

    Making the methods/functions for the customer_handler

--- handlers/customer_handler.py
+++ handlers/customer_handler.py
@@ -1,24 +1,42 @@
-from models.product import Product
+from models.customer import Customer
 from handlers import Session
 
-class ProductHandler:
-    def __init__(self):
-        #Start session here?
-        engine = #Get from function?
-        self.session = engine
+class CustomerHandler:
+    __tablename__ = 'customers'
 
-    def get_all_customers(self):
-        self.session.query(customers).all()
+    def get_all_customers(self, enter_session):
+        enter_session.query(customers).all()
 
-    def add_one_product(self):
-        self.session.query(customers).add_entity()
+    def add_one_customer(self, enter_session):
+        enter_session.query(customers).add_entity()
 
-    def remove(self):
-        self.session.query(customers).delete()
+    def remove(self, enter_session):
+        enter_session.query(customers).delete()
 
-    def search(self, looking_for):
-        self.session.query(customers).get(looking_for)
+    def search(self, enter_session):
+        enter_session.query(customers).get(looking_for)
 
-with Session.begin() as session:
-    session.add()
-    session.commit()
+#NOTE: either add the argument to be modified from class or function?
+def adding_customer(add_customer, enter_session=None):
+    """Starts session to add customer and automatically ends it"""
+    with Session.begin() as session:
+        CustomerHandler.add_one_customer(add_customer, enter_session=session)
+        session.commit()
+
+def showing_all(show_all, enter_session=None):
+    """Starts session to show all customers and automatically ends it"""
+    with Session.begin() as session:
+        CustomerHandler.get_all_customers(enter_session=session)
+        session.commit()
+
+def removing_customer(remove_customer, enter_session=None):
+    """Starts session to remove customer and automatically ends it"""
+    with Session.begin() as session:
+        CustomerHandler.remove(remove_customer, enter_session=session)
+        session.commit()
+
+def show_customer(search_customer, enter_session=None):
+    """Starts session to show a customer and automatically ends it"""
+    with Session.begin() as session:
+        CustomerHandler.search(search_customer, enter_session=session)
+        session.commit()
