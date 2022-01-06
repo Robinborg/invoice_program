dca88feada4675450013bfc25879b65a07b69c4f
Robinborg <coalchewer@robins-macbook-air.home>
2022-01-06 21:06:48 +0100

    Added import path to testing

--- my_test_suite/test.py
+++ my_test_suite/test.py
@@ -1,23 +1,35 @@
-if __name__ == "__main__" and __package__ is not None:
+#if __name__ == "__main__" and __package__ is None:
+#    from sys import path
+#    from os.path import dirname
+#    path.append(dirname(path[0]))
+#    __package__ = "examples"
+#
+if __name__ == "__main__" and __package__ is None:
     import sys
     from os import path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    import customer_interface._customer_menu
+    var = path.dirname(path.abspath(__file__)) + "/../src"
+    print("-"*60)
+    print(var)
+    print("-"*60)
+    sys.path.append(var)
+    print(sys.path)
+    print("-"*60)
 
+import interface.customer_interface
 from unittest import TestCase
 from unittest import mock
 import unittest
 
 class TestInput(TestCase):
     """Test input for menus"""
-    @mock.patch("_customer_menu.input", side_effect=['1', '2', '3', '4', 'q'])
-    def test_customer_menu(self, _customer_menu):
+    @mock.patch("interface.customer_interface._customer_menu.customer_mode", side_effect=['1', '2', '3', '4', 'q'])
+    def test_customer(self, customer_menu):
         """Testing customer_menu"""
-        calling_1 = _customer_menu()
-        calling_2 = _customer_menu()
-        calling_3 = _customer_menu()
-        calling_4 = _customer_menu()
-        calling_q = _customer_menu()
+        calling_1 = interface.customer_interface._customer_menu()
+        calling_2 = interface.customer_interface._customer_menu()
+        calling_3 = interface.customer_interface._customer_menu()
+        calling_4 = interface.customer_interface._customer_menu()
+        calling_q = interface.customer_interface._customer_menu()
         self.assertTrue(calling_1 == '1' and
                         calling_2 == '2' and
                         calling_3 == '3' and
@@ -26,7 +38,7 @@ class TestInput(TestCase):
 
 
 #    @mock.patch("_product_menu.input", side_effect=['1', '2', '3', '4', 'q'])
-#    def test_product_menu(self, _customer_menu):
+#    def test_product_menu(self, ci._customer_menu):
 #        """Testing product menu"""
 #        calling_1 = _product_menu()
 #        calling_2 = _product_menu()
